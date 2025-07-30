94c5ae966cdd0e3b9098ea2c8cd623cb1f31741b
edward <edwardlfish@gmail.com>
2025-07-30 20:52:44 +0300

    Use error page with incorrect userId

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -3,15 +3,16 @@ import {useContext, useEffect, useState} from "react";
 import type {HeroInfo} from "../utils/types";
 import {useParams} from "react-router";
 import {SWContext} from "../utils/context.ts";
+import ErrorPage from "./ErrorPage.tsx";
 
 const AboutMe = () => {
     const [hero, setHero] = useState<HeroInfo>();
-    let {heroId = defaultHero} = useParams();
+    const {heroId = defaultHero} = useParams();
     const {changeHero} = useContext(SWContext);
 
     useEffect(() => {
-        if(!(heroId in characters)){
-            heroId = defaultHero;
+        if (!(heroId in characters)) {
+            return;
         }
         changeHero(heroId);
         const hero = JSON.parse(localStorage.getItem(heroId)!);
@@ -40,17 +41,18 @@ const AboutMe = () => {
         }
     }, [heroId])
 
-    return (
+    return (heroId in characters) ? (
         <>
             {(!!hero) &&
                 <div className={'text-[2em] text-justify tracking-widest leading-14 ml-8'}>
                     {Object.keys(hero).map(key => <p key={key}>
-                        <span className={'text-3xl capitalize'}>{key.replace('_', ' ')}</span>: {hero[key as keyof HeroInfo]}
+                        <span
+                            className={'text-3xl capitalize'}>{key.replace('_', ' ')}</span>: {hero[key as keyof HeroInfo]}
                     </p>)}
                 </div>
             }
         </>
-    );
+    ) : <ErrorPage/>;
 };
 
 export default AboutMe;
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,25 +5,26 @@ import {characters, defaultHero} from "../utils/constants.ts";
 import {useParams} from "react-router";
 import {useContext, useEffect} from "react";
 import {SWContext} from "../utils/context.ts";
+import ErrorPage from "./ErrorPage.tsx";
 
 const Home = () => {
-    let {heroId = defaultHero} = useParams();
+    const {heroId = defaultHero} = useParams();
     const {changeHero} = useContext(SWContext);
 
     useEffect(() => {
-        if(!(heroId in characters)){
-            heroId = defaultHero;
+        if (!(heroId in characters)) {
+            return;
         }
         changeHero(heroId);
     }, [heroId])
 
-    return (
+    return (heroId in characters) ? (
         <main>
             <Hero/>
             <DreamTeam/>
             <OpeningCrawl/>
         </main>
-    );
+    ) : <ErrorPage/>;
 };
 
 export default Home;
